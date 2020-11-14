1aa3264c30ee37ef271119967479d199defbf22a
Robert Perszewski <perszewski.robert@gmail.com>
2020-11-14 17:34:32 +0100

    Add scroll feature

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log("cos");
     const rootElement=document.getElementById("root");
-    const sections= document.querySelectorAll('section');
-    
+    const sections = document.querySelectorAll('section');
+    let currentSectionIndex=0;
+    let isThrottled = false;    
+
     document.addEventListener('mousewheel', function (event){
-        console.log(event.wheelDelta)
+        
+        if (isThrottled) return;
+        isThrottled = true;
+
+        setTimeout(function(){
+            isThrottled = false;
+        }, 750);
+
+        const direction= event.wheelDelta < 0 ? 1 : -1;
+        if(direction === 1){  
+          const isLast = sections.length-1 === currentSectionIndex;
+          if(isLast) return;
+
+        }
+        if(direction === -1){  
+            const isFirst = 0 === currentSectionIndex;
+            if(isFirst) return;
+            }     
+        currentSectionIndex += direction;      
+        console.log(currentSectionIndex)
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
     })
+    
 })
